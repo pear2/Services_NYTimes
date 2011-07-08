59f1d87470d86beae4cfc73c0c320321148175dc
till <till@php.net>
2011-07-08 20:37:40 +0200

    * add files necessary for release

---
+++ packagexmlsetup.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Extra package.xml settings such as dependencies.
+ * More information: http://pear.php.net/manual/en/pyrus.commands.make.php#pyrus.commands.make.packagexmlsetup
+ */
+$package->dependencies['required']->package['pear.php.net/HTTP_Request2']->save();
+?>
