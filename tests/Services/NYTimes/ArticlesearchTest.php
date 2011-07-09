7cc6a2ab24aa06a4f6cf5996aa1a9324db0194ef
till <till@php.net>
2011-07-09 03:33:19 +0200

    skeleton

---
+++ tests/Services/NYTimes/ArticlesearchTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PEAR2\Services\NYTimes\ArticlesearchTest
+ *
+ * PHP version 5
+ *
+ * @category  Services
+ * @package   PEAR2_Services_NYTimes
+ * @author    Till Klampaeckel <till@php.net>
+ * @copyright 2011 Till Klampaeckel
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      https://github.com/pear2/Services_NYTimes
+ */
+
+/**
+ * ArticlesearchTest covers {@link \PEAR2\Services\NYTimes\Articlesearch}.
+ *
+ * @category  Services
+ * @package   PEAR2_Services_NYTimes
+ * @author    Till Klampaeckel <till@php.net>
+ * @copyright 2011 Till Klampaeckel
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      https://github.com/pear2/Services_NYTimes
+ */
+namespace PEAR2\Services\NYTimes;
+class ArticlesearchTest extends \PHPUnit_Framework_TestCase
+{
+}
