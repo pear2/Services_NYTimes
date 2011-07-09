31890a581828d648d7da90866142de6c6515223b
till <till@php.net>
2011-07-09 04:37:16 +0200

    * update .xml  * link correct API docs  * update README file

--- src/Services/NYTimes/Articlesearch.php
+++ src/Services/NYTimes/Articlesearch.php
@@ -22,7 +22,7 @@
  * @copyright 2011 Till Klampaeckel
  * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @link      https://github.com/pear2/Services_NYTimes
- * @link      http://developer.nytimes.com/docs/times_newswire_api/
+ * @link      http://developer.nytimes.com/docs/read/article_search_api
  * @link      http://developer.nytimes.com/attribution
  */
 namespace PEAR2\Services\NYTimes;
